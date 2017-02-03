8bf9935040a0790784fd6a5877e88ae4a5470c06
Eriniax <francisco_godoy1992@hotmail.com>
2017-02-03 16:22:07 -0300

    Commit

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -5,20 +5,64 @@ ej.: "la Resta es 750"*/
 function sumar()
 {	
 	
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno = document.getElementById('numeroUno').value;
+
+	numeroDos = document.getElementById('numeroDos').value;
+
+	alert(("La Suma es") + (" ") + (parseInt(numeroUno) + parseInt(numeroDos))); 
+
+
 }
 
 function restar()
 {
 	
+	
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno = document.getElementById('numeroUno').value;
+
+	numeroDos = document.getElementById('numeroDos').value;
+
+	alert(("La Resta es") + (" ") + ((numeroUno) - (numeroDos))); 
+
+
 }
 
 function multiplicar()
 { 
 	
+
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno = document.getElementById('numeroUno').value;
+
+	numeroDos = document.getElementById('numeroDos').value;
+
+	alert(("La Multiplicacion es") + (" ") + ((numeroUno) * (numeroDos))); 
+
+
 }
 
 function dividir()
 {
 	
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno = document.getElementById('numeroUno').value;
+
+	numeroDos = document.getElementById('numeroDos').value;
+
+	alert(("La Division es") + (" ") + ((numeroUno) / (numeroDos))); 
+
+
 }
 
+
+//Solo la suma se usa parseInt dado que el simbolo + tambien se usa para concatenar 	
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -5,4 +5,33 @@ ej.: "El resto es 0 ."*/
 function SacarResto()
 {
 	
+	var numeroDividendo;
+
+	var numeroDivisor;
+
+	numeroDividendo = document.getElementById('numeroDividendo').value;
+
+	numeroDivisor = document.getElementById("numeroDivisor").value;
+
+	alert(("El resto es") + (" ") + ((numeroDividendo) / (numeroDivisor))); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
