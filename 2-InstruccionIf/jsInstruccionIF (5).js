4e8abb57e0e576c548af81dc7570de0e25064da8
Eriniax <francisco_godoy1992@hotmail.com>
2017-02-09 20:25:50 -0300

    Commit

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -3,4 +3,59 @@ function Mostrar()
 //tomo la edad  
 
 
+	/*var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if (edad<18)
+	{
+		if (edad>13)
+		{
+			alert("No es Adolescente");
+		}
+	}
+*/
+
+
+	/*var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if (edad<18 && edad>12)
+	{
+		
+		alert("Adolescente");
+		
+	}
+
+	else
+	{
+		alert("No es Adolescente");
+	}*/
+
+
+
+var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if (edad<18 && edad>12)
+	{
+		
+		alert("Adolescente");
+		
+	}
+
+	if (!(edad<18 && edad>12))
+	{
+		alert("No es Adolescente");
+	}
+
+
+// Formas de resolverlo 
+
+
+
+
+
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,25 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
 
+	edad = document.getElementById('edad').value;
 
+	if (edad<18 && edad>12)
+	{
+		
+		alert("Adolescente");
+		
+	}
+
+	if (edad>17)
+	{
+		alert("Mayor de edad");
+	}
+
+	if (edad<13)
+	{
+		alert("Niño");
+	}
 
 }//FIN DE LA FUNCIÓN
