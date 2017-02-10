41335055234be74a5d40c464ad9d413e9df2a5d0
Eriniax <francisco_godoy1992@hotmail.com>
2017-02-09 21:30:32 -0300

    Commit

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,22 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+
+	soltero = "Soltero"; //Soltero es una palabra, no una VARIABLE, ya que no cambia
+
+	var estadocivil;
+
+	edad = document.getElementById('edad').value;
+
+	estadocivil = document.getElementById('estadoCivil').value;
+
+	//Esta var la saque del <option value="Soltero"> Soltero </option> para dejarla como texto
+
+	if (edad<18&&estadocivil!=soltero)
+	{
+		alert("Es muy pequeño para No ser soltero");
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,22 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+
+	var estadocivil;
+
+	soltero = "Soltero";
+
+	edad = document.getElementById('edad').value;
+
+	estadocivil = document.getElementById('estadoCivil').value;
+
 	
+
+	if (edad>17&&estadocivil==soltero)
+	{
+		alert("Es soltero y no es menor");
+	}
 
 
 }//FIN DE LA FUNCIÓN
