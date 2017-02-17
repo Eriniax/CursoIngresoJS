79d31e6be00cbc251933117978538336bf1b332c
Eriniax <francisco_godoy1992@hotmail.com>
2017-02-17 11:28:58 -0300

    Commit

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -6,9 +6,125 @@ C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
-
  */
 function CalcularPrecio () 
 {
+	var precioBruto;
+	var descuento;
+	var precioNeto;
+ 	var cantidad=document.getElementById('Cantidad').value;
+    var marca=document.getElementById('Marca').value;
+	
+
+	 precioBruto=35*cantidad;
  	
+ 	switch(marca)
+ 	{
+
+ 		case "ArgentinaLuz":
+
+ 		    if(cantidad==3)
+ 		    { 		    	
+ 		    	 descuento=0.85; 
+            }
+            else
+            {
+            	if(cantidad==4)
+            	{
+ 		    	    descuento=0.75;
+            	}
+            	else
+            	{
+	            	if(cantidad==5)
+	            	{	            		
+	 		    	     descuento=0.60;
+	 		    	}
+	 		    	else
+	 		    	{
+	 		    		 descuento=0.50;
+
+	 		    	}
+            	}
+
+            }
+
+ 		break;
+
+ 		case "FelipeLamparas":
+
+ 		    if(cantidad==3)
+ 		    {
+ 		    	descuento=0.90;
+ 		    }
+ 		    else
+ 		    {
+ 		    	if(cantidad==4)
+ 		    	{
+ 		    		descuento=0.75;
+ 		    	}
+ 		    	else
+ 		    	{
+ 		    		if(cantidad==5)
+ 		    		{
+ 		    			descuento=0.70;
+ 		    		}
+ 		    		else
+ 		    		{
+ 		    			descuento=0.50;
+ 		    		}
+ 		    	}
+ 		    }
+ 		break;
+
+ 		default:
+ 		   if(cantidad==3)
+ 		   {
+ 		   	descuento=0.95;
+ 		   }
+ 		   else
+ 		   {
+ 		   	if(cantidad==4)
+ 		   	{
+ 		   		descuento=0.80;
+ 		   	}
+ 		   	else
+ 		   	{
+ 		   		if (cantidad==5)
+ 		   		{
+ 		   			descuento=0.70;
+ 		   		}
+ 		   		else
+ 		   		{
+ 		   			descuento=0.50;
+ 		   		}
+ 		   	}
+ 		   }
+
+ 	}//switch(marca)
+
+ 	precioNeto=precioBruto*descuento;
+
+
+	document.getElementById('precioDescuento').value=precioNeto;
+
+}//function CalcularPrecio
+
+
+
+
+/*if(cantidad>=6) otra forma de hacerlo
+{
+	descuento=0.5;
 }
+else
+{
+	if(cantidad==5)
+	{
+		switch(marca)
+		{
+			case "ArgentinaLuz"
+			  descuento=0.6;
+			break;
+		}
+	}
+}*/
